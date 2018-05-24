989408b0ac12017b07cba77fc8ecb2d7fe839f0e
Abdullah Al Amin <aaa006bd@gmail.com>
2018-05-24 09:38:52 +0600

    2nd update

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,4 +1,5 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { reject } from 'q';
 
 
 export class UsernameValidators {
@@ -10,5 +11,17 @@ export class UsernameValidators {
       };
     }
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'mosh') {
+          resolve({shouldBeUnique: true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
   }
 }
